2184e32f77bf92cf022322187b913c73fd73b4e3
Larry Amaral Reis <lar.snf21@uea.edu.br>
2022-02-18 02:49:16 -0300

    Início de jogo + Interações com Sons

--- breakout.py
+++ breakout.py
@@ -26,20 +26,20 @@ for i in range(28): blocks.append(["#c6c811", True])
 for i in range(8):
     # collumns
     for j in range(14):
-        id = (i*14) + j
+        id = (i * 14) + j
         # block position
-        blocks[id].append(8 + (j*35)) # x position
-        blocks[id].append(133 + (i*13)) # y position
+        blocks[id].append(8 + (j * 35))  # x position
+        blocks[id].append(133 + (i * 13))  # y position
         # height is always 8 and width is always 30
 
 # Load Player and ball
-player_surf = pygame.image.load('graphics/player.png').convert()
-player_rect = player_surf.get_rect(midbottom = (250, 620))
+player_surf = pygame.image.load('graphics/initial.png').convert()
+player_rect = player_surf.get_rect(midbottom=(250, 620))
 player_moving_left = False
 player_moving_right = False
 
 ball_surf = pygame.image.load('graphics/ball.png').convert()
-ball_rect = ball_surf.get_rect(midbottom = (WIDTH/2, 580))
+ball_rect = ball_surf.get_rect(midbottom=(200000, 200000))
 ball_velocity = 3
 ball_dx = ball_velocity
 ball_dy = ball_velocity * (-1)
@@ -57,7 +57,7 @@ def player_update():
         player_rect.x += 7
     if player_moving_left:
         player_rect.x -= 7
-    
+
     # Walls collision
     if player_rect.left <= 0:
         player_rect.left = 0
@@ -75,17 +75,27 @@ def ball_update():
     # Walls collision
     if ball_rect.top <= 20:
         ball_dy = ball_velocity
+        pygame.mixer.music.load('sounds/sound_1.mp3')
+        pygame.mixer.music.play(0)
     if ball_rect.bottom >= HEIGHT:
         ball_dy = ball_velocity * (-1)
+        pygame.mixer.music.load('sounds/sound_2.mp3')
+        pygame.mixer.music.play(0)
     if ball_rect.left <= 0:
         ball_dx = ball_velocity
+        pygame.mixer.music.load('sounds/sound_1.mp3')
+        pygame.mixer.music.play(0)
     if ball_rect.right >= WIDTH:
         ball_dx = ball_velocity * (-1)
-    
+        pygame.mixer.music.load('sounds/sound_1.mp3')
+        pygame.mixer.music.play(0)
+
     # Paddle collision
     if ball_rect.colliderect(player_rect):
         ball_dy = ball_velocity * (-1)
-    
+        pygame.mixer.music.load('sounds/sound_1.mp3')
+        pygame.mixer.music.play(0)
+
     # Collision with a block
     if block_collision():
         ball_dy = ball_velocity
@@ -97,6 +107,9 @@ def block_collision():
         # check collision
         if ball_rect.colliderect(pygame.Rect((blocks[i][2], blocks[i][3]), (30, 8))) and blocks[i][1]:
             blocks[i][1] = False
+            if blocks[i][1] == False:
+                pygame.mixer.music.load('sounds/sound_1.mp3')
+                pygame.mixer.music.play(0)
             return True
 
 # Game loop
@@ -109,6 +122,12 @@ while True:
             # close de program
             exit()
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_surf = pygame.image.load('graphics/player.png').convert()
+                player_rect = player_surf.get_rect(midbottom=(250, 620))
+                ball_rect = ball_surf.get_rect(midbottom=(WIDTH / 2, 350))
+
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 player_moving_right = True
             if event.key == pygame.K_LEFT:
@@ -118,7 +137,7 @@ while True:
                 player_moving_right = False
             if event.key == pygame.K_LEFT:
                 player_moving_left = False
-    
+
     # Load background
     screen.blit(back_img, (0, 0))
 
@@ -126,25 +145,25 @@ while True:
     for i in range(8):
         # collumns
         for j in range(14):
-            block = blocks[(i*14) + j]
+            block = blocks[(i * 14) + j]
             # if the block isn't broken
             if block[1]:
-                pygame.draw.line(screen, block[0], (8 + (j*35), 133 + (i*13)), (38 + (j*35), 133 + (i*13)), 8)
-    
+                pygame.draw.line(screen, block[0], (8 + (j * 35), 133 + (i * 13)), (38 + (j * 35), 133 + (i * 13)), 8)
+
     # Score
     screen.blit(round_surf, (40, 25))
     screen.blit(round_surf, (320, 25))
 
     screen.blit(score_surf, (45, 85))
     screen.blit(score_surf, (325, 85))
-    
+
     # Player and ball
     screen.blit(player_surf, player_rect)
     screen.blit(ball_surf, ball_rect)
 
     player_update()
     ball_update()
-    
+
     pygame.display.update()
 
     # makes sure that the loop won't run faster than
